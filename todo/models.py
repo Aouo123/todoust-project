e529ef778ac3665621ccbda27d50353df32ad664
Ben <magician9457@gmail.com>
2025-05-09 19:46:30 +0800

    新增Todo資料模型

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField( max_length = 100 )
+    text = models.TextField( blank = True ) #blank=True 可以為空字串
+    created = models.DateTimeField( auto_now_add = True ) #auto_now_add(建立時自動填上目前時間)
+    date_completed = models.DateTimeField( null = True, blank = True ) #null=True(可以為空物件型態)
+    important = models.BooleanField( default = False)
+
+    def __str__(self):
+        return f"{self.id} - {self.title} - {self.created}"
+    

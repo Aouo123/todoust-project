35a9d102b346279cb0d660d831aa5e1a2b996915
Ben <magician9457@gmail.com>
2025-05-28 20:43:33 +0800

    測試

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c*mr-(1&_sd!n$q&p$(-tq!al$lv0q(@01p(_7etf*_)yn0^=r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
